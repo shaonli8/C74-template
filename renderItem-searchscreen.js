8a32adb4386324117cda9750775918033841c3a1
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2022-01-08 11:19:59 +0530

    Create renderItem-searchscreen.js

---
+++ renderItem-searchscreen.js
@@ -0,0 +1,56 @@
+renderItem = ({ item, i }) => {
+    var date = item.date
+      .toDate()
+      .toString()
+      .split(" ")
+      .splice(0, 4)
+      .join(" ");
+
+    var transactionType =
+      item.transaction_type === "issue" ? "issued" : "returned";
+    return (
+      <View style={{ borderWidth: 1 }}>
+        <ListItem key={i} bottomDivider>
+          <Icon type={"antdesign"} name={"book"} size={40} />
+          <ListItem.Content>
+            <ListItem.Title style={styles.title}>
+              {`${item.book_name} ( ${item.book_id} )`}
+            </ListItem.Title>
+            <ListItem.Subtitle style={styles.subtitle}>
+              {`This book ${transactionType} by ${item.student_name}`}
+            </ListItem.Subtitle>
+            <View style={styles.lowerLeftContaiiner}>
+              <View style={styles.transactionContainer}>
+                <Text
+                  style={[
+                    styles.transactionText,
+                    {
+                      color:
+                        item.transaction_type === "issue"
+                          ? "#78D304"
+                          : "#0364F4"
+                    }
+                  ]}
+                >
+                  {item.transaction_type.charAt(0).toUpperCase() +
+                    item.transaction_type.slice(1)}
+                </Text>
+                <Icon
+                  type={"ionicon"}
+                  name={
+                    item.transaction_type === "issue"
+                      ? "checkmark-circle-outline"
+                      : "arrow-redo-circle-outline"
+                  }
+                  color={
+                    item.transaction_type === "issue" ? "#78D304" : "#0364F4"
+                  }
+                />
+              </View>
+              <Text style={styles.date}>{date}</Text>
+            </View>
+          </ListItem.Content>
+        </ListItem>
+      </View>
+    );
+  };
