7395a4348b558fca2f1e41d6034752f85a2803a4
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2022-01-08 11:19:08 +0530

    Create searchscreen-stylesheet.js

---
+++ searchscreen-stylesheet.js
@@ -0,0 +1,75 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5653D4"
+  },
+  upperContainer: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF"
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF"
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  lowerContainer: {
+    flex: 0.8,
+    backgroundColor: "#FFFFFF"
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  lowerLeftContaiiner: {
+    alignSelf: "flex-end",
+    marginTop: -40
+  },
+  transactionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  transactionText: {
+    fontSize: 20,
+
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  date: {
+    fontSize: 12,
+    fontFamily: "Rajdhani_600SemiBold",
+    paddingTop: 5
+  }
+});
